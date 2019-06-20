daa7b898871a503bdfc38b641bd9394f7434b458
faisalkanout <f.kanout@gmail.com>
2019-06-20 13:44:30 +0200

    UPD - now the body is parsed based on predefined element

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Crawler = require('crawler')
 const request = require('axios')
 const uri = 'https://developer.axway.com'
+const excludedElements = ['body', 'div', 'ui', 'script', 'style', 'head', 'html']
 var c = new Crawler({
   maxConnections: 40,
   // This will be called for each crawled page
@@ -25,19 +26,31 @@ c.queue([
         return console.log(error)
       }
       const $ = res.$
-      let bodyContent
       $('*').each((i, elem) => {
-        if (elem.name === 'body') {
-          const rawBodyContent = $(elem)
-            .text()
-            .trim()
-          bodyContent = rawBodyContent.replace(/\s+/g, ' ')
+        if (!excludedElements.includes(elem.name)) {
+          const rawText = $(elem).text().trim()
+          const text = rawText.replace(/\s+/g, ' ')
+          if (!text) {
+            return
+          }
           const indexObject = {
             href: uri,
-            text: bodyContent
+            text
           }
           index(indexObject)
         }
+        // let bodyContent
+        // if (elem.name === 'body') {
+        //   const rawBodyContent = $(elem)
+        //     .text()
+        //     .trim()
+        //   bodyContent = rawBodyContent.replace(/\s+/g, ' ')
+        //   const indexObject = {
+        //     href: uri,
+        //     text: bodyContent
+        //   }
+        //   index(indexObject)
+        // }
         if (elem.name === 'a') {
           const href = $(elem).attr('href')
           const title = $(elem).text()
